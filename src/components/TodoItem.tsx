49f2f55e831a2072e516b1ef7c24342f0616be70
Yury <yurydgfuentes@gmail.com>
2020-10-22 23:33:40 +0300

    Add component TodoItem

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const TodoItem = () => {
+  const [data, setData] = useState(0);
+  return (
+    <div>TodoItem component</div>
+  )
+}
+
+export default TodoItem;
