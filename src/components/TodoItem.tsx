11f4a1210c8ace9a2a4a44092160b5483e0b08f6
Eduardo Noel Rosales <erosales811@gmail.com>
2020-10-22 16:57:53 -0400

    Adding id prop

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const TodoItem = (props: any) => {
-  return <div>{props.title}</div>;
+  return (
+    <>
+      <div>{props.title}</div>
+      <div>{props.id}</div>
+    </>
+  );
 };
 
 export default TodoItem;
